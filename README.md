275e54571d4e9fe31d45c5190f150ebe14cb4f00
Kettil <development@kettil.de>
2020-05-08 23:37:46 +0200

    🐣

---
+++ README.md
@@ -0,0 +1,19 @@
+# semantic-release-config
+
+The library contains the semantic-release configuration.
+
+## Installation
+
+```bash
+npm install eslint @kettil/semantic-release-config
+```
+
+## Quick Start
+
+To use this configuration, create the file `.releaserc.json` with the following content:
+
+```json
+{
+  "extends": "@kettil/semantic-release-config"
+}
+```
---
+++ configuration.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-template-curly-in-string */
+
+const configuration = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+
+  tagFormat: '${version}',
+};
+
+module.exports = configuration;
