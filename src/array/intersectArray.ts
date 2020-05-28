5df58f84d3d2a51ff7a8dd8ac089ba9d1759591a
Kettil <development@kettil.de>
2020-05-29 00:00:03 +0200

    style(array): Convert function to arrow function

--- src/array/intersectArray.ts
+++ src/array/intersectArray.ts
@@ -1,5 +1,3 @@
-const intersectArray = <T>(array1: T[], array2: T[]): T[] => {
-  return array1.filter((key) => array2.includes(key));
-};
+const intersectArray = <T>(array1: T[], array2: T[]): T[] => array1.filter((key) => array2.includes(key));
 
 export default intersectArray;
