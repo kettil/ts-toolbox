5fc2611571987a3d051a950ca9596a3303243194
Kettil <development@kettil.de>
2020-07-06 13:55:18 +0200

    refactor(node): update the delay function
    Better handling of the parameter "ms" with less than or equal to 0.

--- src/node/delay.test.ts
+++ src/node/delay.test.ts
@@ -8,4 +8,20 @@ describe('arrayUnique()', () => {
 
     expect(Date.now() - now).toBeGreaterThanOrEqual(432);
   });
+
+  test('it should work with equal 0', async () => {
+    const now = Date.now();
+
+    await delay(0);
+
+    expect(Date.now() - now).toBeLessThan(5);
+  });
+
+  test('it should work with less than 0', async () => {
+    const now = Date.now();
+
+    await delay(-5);
+
+    expect(Date.now() - now).toBeLessThan(5);
+  });
 });
--- src/node/delay.ts
+++ src/node/delay.ts
@@ -1,3 +1,9 @@
-const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, Math.max(Math.ceil(ms), 0)));
+const delay = (ms: number): Promise<void> => {
+  if (ms > 0) {
+    return new Promise((resolve) => setTimeout(resolve, Math.ceil(ms)));
+  }
+
+  return Promise.resolve();
+};
 
 export default delay;
