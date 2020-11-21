8c90f51364e45d380096be54dfebaa52aff4cc9d
Kjell Dießel <development@kettil.de>
2020-11-21 15:51:48 +0100

    fix(formatjs): rules are only loaded when needed

--- plugins/formatjs.js
+++ plugins/formatjs.js
@@ -1,11 +1,16 @@
 const defaultObject = require('../lib/defaultObject');
+const hasLibrary = require('../lib/hasLibrary');
 
-const rules = {
-  'formatjs/enforce-description': ['off'],
-  'formatjs/enforce-placeholders': ['error'],
-  'formatjs/enforce-plural-rules': ['error'],
-  'formatjs/no-emoji': ['error'],
-  'formatjs/no-offset': ['error'],
-};
+if (hasLibrary('react-intl')) {
+  const rules = {
+    'formatjs/enforce-description': ['off'],
+    'formatjs/enforce-placeholders': ['error'],
+    'formatjs/enforce-plural-rules': ['error'],
+    'formatjs/no-emoji': ['error'],
+    'formatjs/no-offset': ['error'],
+  };
 
-module.exports = { ...defaultObject, plugins: ['formatjs'], rules };
+  module.exports = { ...defaultObject, plugins: ['formatjs'], rules };
+} else {
+  module.exports = defaultObject;
+}
