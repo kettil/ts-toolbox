11b507fb35fec4806bb4d9dd828b7380df70d493
Kjell Dießel <development@kettil.de>
2020-12-02 16:49:23 +0100

    feat(jest-dom): add rule jest-dom/prefer-to-have-value

--- plugins/testingLibraryJestDom.js
+++ plugins/testingLibraryJestDom.js
@@ -10,6 +10,7 @@ const rules = {
   'jest-dom/prefer-required': ['error'],
   'jest-dom/prefer-to-have-attribute': ['error'],
   'jest-dom/prefer-to-have-text-content': ['error'],
+  'jest-dom/prefer-to-have-value': ['error'],
 };
 
 if (hasLibrary('@testing-library/jest-dom')) {
