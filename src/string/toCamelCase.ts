591aa4a466c3e95b9c8afc2d8655ece84b4320b2
Kjell Dießel <development@kettil.de>
2020-12-08 23:36:04 +0100

    style(string): indentation

--- src/string/toCamelCase.ts
+++ src/string/toCamelCase.ts
@@ -1,8 +1,9 @@
-const toCamelCase = (value: string): string => value
-  .replace(/([A-Z])/g, ' $1')
-  .trim()
-  .split(/[^\da-z]/i)
-  .map((v, i) => (i === 0 ? v[0].toLowerCase() : v[0].toUpperCase()) + v.slice(1).toLowerCase())
-  .join('');
+const toCamelCase = (value: string): string =>
+  value
+    .replace(/([A-Z])/g, ' $1')
+    .trim()
+    .split(/[^\da-z]/i)
+    .map((v, i) => (i === 0 ? v[0].toLowerCase() : v[0].toUpperCase()) + v.slice(1).toLowerCase())
+    .join('');
 
 export default toCamelCase;
