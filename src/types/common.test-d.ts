de479987b8e787f6d09ea368c735a4a5970e996d
Kjell Dießel <development@kettil.de>
2021-03-04 19:48:48 +0100

    fix(types): fix the type equality test

--- src/types/common.test-d.ts
+++ src/types/common.test-d.ts
@@ -8,7 +8,7 @@ export type EqualType1Test = Expect<EqualType1Value, true, true>;
 type EqualType2Value = EqualType<{ a: number }, { a: number }, false>;
 export type EqualType2Test = Expect<EqualType2Value, true, true>;
 
-type EqualType3Value = EqualType<{ a: number }, { a: false }, false>;
+type EqualType3Value = EqualType<{ a: number }, { a: string }, false>;
 export type EqualType3Test = Expect<EqualType3Value, true, false>;
 
 // TupleOf
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,12 @@
-export type EqualType<T, S, R extends never | false = never> = [T] extends [S] ? ([S] extends [T] ? true : R) : R;
+type EqualType1<T, S> = [T] extends [S] ? ([S] extends [T] ? true : false) : false;
+
+type EqualType2<T, S> = (<X>() => X extends T ? 1 : 2) extends <X>() => X extends S ? 1 : 2 ? true : false;
+
+export type EqualType<T, S, R extends never | false = never> = EqualType1<T, S> extends true
+  ? EqualType2<T, S> extends true
+    ? true
+    : R
+  : R;
 
 export type Expect<Value, Expected, Result extends EqualType<Value, Expected, false>> = Result;
 
