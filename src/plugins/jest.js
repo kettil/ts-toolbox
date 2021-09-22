3a8dddbe92796e302822967fd402c03235ebbb4e
Kjell Dießel <development@kettil.de>
2021-09-22 15:29:03 +0200

    feat(jest): add rule max-nested-describe

--- src/plugins/jest.js
+++ src/plugins/jest.js
@@ -8,6 +8,7 @@ const rules = {
   'jest/consistent-test-it': ['error', { fn: 'test', withinDescribe: 'test' }],
   'jest/expect-expect': ['warn'],
   'jest/lowercase-name': ['error'],
+  'jest/max-nested-describe': ['error', { max: 5 }],
   'jest/no-alias-methods': ['error'],
   'jest/no-commented-out-tests': ['warn'],
   'jest/no-conditional-expect': ['error'],
