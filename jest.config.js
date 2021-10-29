066b302c5042d385c27d3e8d47e86ee018c963b5
Kjell Dießel <development@kettil.de>
2021-10-29 10:10:51 +0200

    chore: update the configs for the carna v5 update

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,10 @@
-// eslint-disable-next-line import/no-extraneous-dependencies -- is ok, because config file
-const { config, createProject } = require('carna/configs/jest.config.js');
+/* eslint-disable-next-line import/no-extraneous-dependencies -- is a dev file */
+const { createJestConfig, createJestProject } = require('carna');
 
-module.exports = Object.assign(config, {
+module.exports = createJestConfig(__dirname, {
   projects: [
-    // createProject('<foldername>', '<color>', { /* extend config */})
     // => colors: red green yellow blue magenta cyan gray
-    createProject('unit', 'cyan', {}),
+
+    createJestProject({ color: 'cyan', testFolder: 'unit', customeConfig: {} }),
   ],
-
-  // extend options for this project
-
-  coverageThreshold: {
-    global: {
-      statements: 50,
-      branches: 50,
-      functions: 50,
-      lines: 50,
-    },
-  },
 });
