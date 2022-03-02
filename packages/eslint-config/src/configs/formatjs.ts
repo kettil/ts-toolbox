e0cbfa5c2b996d8928d8699e278fc68a48e69a3a
Kjell Dießel <development@kettil.de>
2022-03-02 11:17:57 +0100

    feat(eslint-config): missing formatjs rules added and generally adjusted
    BREAKING CHANGE: The missing formatjs rules have been added and a few rules have been adjusted where by the code needs to be modified.

--- packages/eslint-config/src/configs/formatjs.ts
+++ packages/eslint-config/src/configs/formatjs.ts
@@ -4,11 +4,18 @@ import type { DefaultObject } from '../types/defaultObject';
 import type { Rules } from '../types/rules';
 
 const rules: Rules = {
+  'formatjs/blocklist-elements': ['off'],
+  'formatjs/enforce-default-message': ['off'],
   'formatjs/enforce-description': ['off'],
+  'formatjs/enforce-id': ['off'],
   'formatjs/enforce-placeholders': ['error'],
   'formatjs/enforce-plural-rules': ['error'],
+  'formatjs/no-camel-case': ['error'],
   'formatjs/no-complex-selectors': ['error', { limit: 3 }],
   'formatjs/no-emoji': ['error'],
+  'formatjs/no-id': ['off'],
+  'formatjs/no-multiple-plurals': ['off'],
+  'formatjs/no-multiple-whitespaces': ['off'],
   'formatjs/no-offset': ['error'],
 };
 
@@ -25,4 +32,4 @@ const createFormatjsConfig = (): DefaultObject => {
   return config;
 };
 
-export { createFormatjsConfig };
+export { createFormatjsConfig, rules as formatjsRules };
