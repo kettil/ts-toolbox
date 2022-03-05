4e264565990b197698876ddd587a9c19e48d7eb0
Kjell Dießel <development@kettil.de>
2022-03-05 17:33:53 +0100

    feat(eslint-config): missing testing-library rules added and generally adjusted
    BREAKING CHANGE: The missing testing-library rules have been added and a few rules have been adjusted where by the code needs to be modified.

--- packages/eslint-config/src/configs/testingLibrary.ts
+++ packages/eslint-config/src/configs/testingLibrary.ts
@@ -6,18 +6,28 @@ import type { Rules } from '../types/rules';
 const rules: Rules = {
   'testing-library/await-async-query': ['error'],
   'testing-library/await-async-utils': ['error'],
+  'testing-library/await-fire-event': ['off'],
   'testing-library/consistent-data-testid': ['error'],
+  'testing-library/no-await-sync-events': ['off'],
   'testing-library/no-await-sync-query': ['error'],
   'testing-library/no-container': ['error'],
   'testing-library/no-debugging-utils': ['off'],
+  'testing-library/no-dom-import': ['off'],
   'testing-library/no-manual-cleanup': ['error'],
   'testing-library/no-node-access': ['error'],
+  'testing-library/no-promise-in-fire-event': ['off'],
   'testing-library/no-render-in-setup': ['error'],
+  'testing-library/no-unnecessary-act': ['off'],
   'testing-library/no-wait-for-empty-callback': ['error'],
   'testing-library/no-wait-for-multiple-assertions': ['error'],
   'testing-library/no-wait-for-side-effects': ['error'],
   'testing-library/no-wait-for-snapshot': ['error'],
+  'testing-library/prefer-explicit-assert': ['off'],
+  'testing-library/prefer-find-by': ['off'],
+  'testing-library/prefer-presence-queries': ['off'],
+  'testing-library/prefer-query-by-disappearance': ['off'],
   'testing-library/prefer-screen-queries': ['error'],
+  'testing-library/prefer-user-event': ['off'],
   'testing-library/prefer-wait-for': ['error'],
   'testing-library/render-result-naming-convention': ['error'],
 };
@@ -42,4 +52,4 @@ const createTestingLibraryConfig = (): DefaultObject => {
   return config;
 };
 
-export { createTestingLibraryConfig };
+export { createTestingLibraryConfig, rules as testingLibraryRules };
