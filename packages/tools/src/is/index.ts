0cfb0525daa9ac04562a1041a7259c0bce5bb67a
Kjell Dießel <development@kettil.de>
2022-03-09 00:06:27 +0100

    feat(tools): add isFunction function

--- packages/tools/src/is/index.ts
+++ packages/tools/src/is/index.ts
@@ -1,4 +1,5 @@
 export { isArray } from './isArray';
+export { isFunction } from './isFunction';
 export { isNil } from './isNil';
 export { isObject } from './isObject';
 export { isString } from './isString';
