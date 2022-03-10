7342fb85533198d36bfbebfbf9baeb4920d28a12
Kjell Dießel <development@kettil.de>
2022-03-11 00:31:40 +0100

    feat(tools): extends Unpacked type with Set and Promise

--- packages/tools/tests/type/types/unpacked.test.ts
+++ packages/tools/tests/type/types/unpacked.test.ts
@@ -6,3 +6,9 @@ export type Unpacked1Test = Expect<Unpacked1Value, string, true>;
 
 type Unpacked2Value = Unpacked<Array<{ a: string; z: number }>>;
 export type Unpacked2Test = Expect<Unpacked2Value, { a: string; z: number }, true>;
+
+type Unpacked3Value = Unpacked<Promise<{ a: string; z: number }>>;
+export type Unpacked3Test = Expect<Unpacked3Value, { a: string; z: number }, true>;
+
+type Unpacked4Value = Unpacked<Set<{ a: string; z: number }>>;
+export type Unpacked4Test = Expect<Unpacked4Value, { a: string; z: number }, true>;
