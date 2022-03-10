ececb7f41dd00917c281e64fd17eee5f14878db5
Kjell Dießel <development@kettil.de>
2022-03-10 23:17:27 +0100

    refactor(tools): code optimization from lowerCaseFirst

--- packages/tools/tests/unit/string/lowercaseFirst.test.ts
+++ packages/tools/tests/unit/string/lowercaseFirst.test.ts
@@ -1,26 +1,26 @@
-import { lowercaseFirst } from '../../../src/string/lowercaseFirst';
+import { lowerCaseFirst } from '../../../src/string/lowerCaseFirst';
 
 describe('lowercaseFirst()', () => {
   test('it should work', () => {
-    const result = lowercaseFirst('Lorem');
+    const result = lowerCaseFirst('Lorem');
 
     expect(result).toBe('lorem');
   });
 
   test('it should work with empty string', () => {
-    const result = lowercaseFirst('');
+    const result = lowerCaseFirst('');
 
     expect(result).toBe('');
   });
 
   test('it should work with upper chars', () => {
-    const result = lowercaseFirst('LOREM');
+    const result = lowerCaseFirst('LOREM');
 
     expect(result).toBe('lOREM');
   });
 
   test('it should work with lower chars', () => {
-    const result = lowercaseFirst('lorem');
+    const result = lowerCaseFirst('lorem');
 
     expect(result).toBe('lorem');
   });
