f7bb6ed0b270c3efedaaed2749d8fbb83bee310d
Kjell Dießel <development@kettil.de>
2022-03-10 22:58:59 +0100

    feat(tools): add kebabCase function

--- packages/tools/src/string/index.ts
+++ packages/tools/src/string/index.ts
@@ -1,5 +1,6 @@
 export { camelCase } from './camelCase';
 export { dotCase } from './dotCase';
 export { indent } from './indent';
+export { kebabCase } from './kebabCase';
 export { lowercaseFirst } from './lowercaseFirst';
 export { uppercaseFirst } from './uppercaseFirst';
