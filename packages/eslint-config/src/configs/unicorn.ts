2f9fca6e7115b5724796e12ea8573245074307de
Kjell Dießel <development@kettil.de>
2022-04-05 23:57:18 +0200

    feat(eslint-config): add the rule unicorn/no-useless-switch-case

--- packages/eslint-config/src/configs/unicorn.ts
+++ packages/eslint-config/src/configs/unicorn.ts
@@ -53,6 +53,7 @@ const rules: Rules = {
   'unicorn/no-useless-length-check': ['error'],
   'unicorn/no-useless-promise-resolve-reject': ['error'],
   'unicorn/no-useless-spread': ['error'],
+  'unicorn/no-useless-switch-case': ['error'],
   'unicorn/no-useless-undefined': ['error'],
   'unicorn/no-zero-fractions': ['error'],
   'unicorn/number-literal-case': ['error'],
