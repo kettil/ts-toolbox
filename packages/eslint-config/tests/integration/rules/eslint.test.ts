988816b161a59ca4cd80a0abd948a4b8cf88f0a6
Kjell Dießel <development@kettil.de>
2022-04-26 15:33:07 +0200

    test(eslint-config): fix an wrong import

--- packages/eslint-config/tests/integration/rules/eslint.test.ts
+++ packages/eslint-config/tests/integration/rules/eslint.test.ts
@@ -1,10 +1,11 @@
-// @ts-expect-error -- Type information is not needed, because test file
-import rules from 'eslint/lib/rules';
+import { Linter } from 'eslint';
 import { eslintRules } from '../../../src/configs/eslint';
 
 describe('eslint', () => {
   test('exist outdated rules', () => {
-    const existedRules = new Set((rules as Map<string, unknown>).keys());
+    const linter = new Linter();
+
+    const existedRules = new Set(linter.getRules().keys());
     const definedRules = Object.keys(eslintRules);
 
     const diffRules = definedRules.filter((rule) => !existedRules.has(rule));
@@ -13,7 +14,9 @@ describe('eslint', () => {
   });
 
   test('exist new rules', () => {
-    const existedRules = [...(rules as Map<string, unknown>).keys()];
+    const linter = new Linter();
+
+    const existedRules = [...linter.getRules().keys()];
     const definedRules = new Set(Object.keys(eslintRules));
 
     const diffRules = existedRules.filter((rule) => !definedRules.has(rule));
